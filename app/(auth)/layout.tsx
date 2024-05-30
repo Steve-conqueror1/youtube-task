3bd025ef41718af12a22808a367bf5dcd5d573d4
Steve-conqueror1 <stephenkilonzo22@yahoo.com>
2024-05-30 12:48:56 +0000

    Build: (6ec4575) app workflow ~ fixed output export

--- app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <main className={"h-screen w-screen bg-gray-300"}>{children}</main>;
-}
